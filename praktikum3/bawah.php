543329417369959c0dbfcf180c0ea5888aa9dd17
dimaskurniawan2k15 <dimas.kurniawan11012004@gmail.com>
2022-03-20 20:52:09 +0700

    upload praktikum 3

---
+++ praktikum3/bawah.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>Create by mahasiswa <a href="https://elena.nurulfikri.ac.id" target="_blank">STT NF</a>2021</p>
+    </footer>
+</body>
+</html>
---
+++ praktikum3/tampilan.php
@@ -0,0 +1,7 @@
+<?php
+include_once 'atas.php';
+?>
+<h1>Welcome to my slog</h1>
+<?php
+require_once 'bawah.php';
+?>
